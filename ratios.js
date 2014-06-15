7cc3c0532ba87e7c10c91d9d86d1099e0e11f6ee
notrom <tim.ian.morton@gmail.com>
2014-06-15 22:43:07 +1200

    pages files

---
+++ ratios.js
@@ -0,0 +1,659 @@
+var ratios = [
+	{
+		"_id" : "Elvis Presley",
+		"value" : {
+			"artistName" : "Elvis Presley",
+			"totalWordCount" : 129092,
+			"uniqueWordCount" : 4527,
+			"ratio" : 0.03506801350974499
+		}
+	},
+	{
+		"_id" : "James Brown",
+		"value" : {
+			"artistName" : "James Brown",
+			"totalWordCount" : 36435,
+			"uniqueWordCount" : 1342,
+			"ratio" : 0.03683271579525182
+		}
+	},
+	{
+		"_id" : "The Rolling Stones",
+		"value" : {
+			"artistName" : "The Rolling Stones",
+			"totalWordCount" : 111715,
+			"uniqueWordCount" : 4809,
+			"ratio" : 0.04304703934118068
+		}
+	},
+	{
+		"_id" : "The Beatles",
+		"value" : {
+			"artistName" : "The Beatles",
+			"totalWordCount" : 55745,
+			"uniqueWordCount" : 2681,
+			"ratio" : 0.048093999461835145
+		}
+	},
+	{
+		"_id" : "Jimi Hendrix",
+		"value" : {
+			"artistName" : "Jimi Hendrix",
+			"totalWordCount" : 22426,
+			"uniqueWordCount" : 1351,
+			"ratio" : 0.06024257558191385
+		}
+	},
+	{
+		"_id" : "Van Morrison",
+		"value" : {
+			"artistName" : "Van Morrison",
+			"totalWordCount" : 58863,
+			"uniqueWordCount" : 4037,
+			"ratio" : 0.06858298081986987
+		}
+	},
+	{
+		"_id" : "Foo Fighters",
+		"value" : {
+			"artistName" : "Foo Fighters",
+			"totalWordCount" : 24343,
+			"uniqueWordCount" : 1710,
+			"ratio" : 0.07024606663106438
+		}
+	},
+	{
+		"_id" : "Aretha Franklin",
+		"value" : {
+			"artistName" : "Aretha Franklin",
+			"totalWordCount" : 26019,
+			"uniqueWordCount" : 1863,
+			"ratio" : 0.07160152196471808
+		}
+	},
+	{
+		"_id" : "Prince",
+		"value" : {
+			"artistName" : "Prince",
+			"totalWordCount" : 67590,
+			"uniqueWordCount" : 4876,
+			"ratio" : 0.07214084923805297
+		}
+	},
+	{
+		"_id" : "Michael Jackson",
+		"value" : {
+			"artistName" : "Michael Jackson",
+			"totalWordCount" : 38321,
+			"uniqueWordCount" : 2777,
+			"ratio" : 0.07246679366404843
+		}
+	},
+	{
+		"_id" : "Eric Clapton",
+		"value" : {
+			"artistName" : "Eric Clapton",
+			"totalWordCount" : 35491,
+			"uniqueWordCount" : 2576,
+			"ratio" : 0.07258178129666676
+		}
+	},
+	{
+		"_id" : "Metallica",
+		"value" : {
+			"artistName" : "Metallica",
+			"totalWordCount" : 39317,
+			"uniqueWordCount" : 2892,
+			"ratio" : 0.07355596815626828
+		}
+	},
+	{
+		"_id" : "Madonna",
+		"value" : {
+			"artistName" : "Madonna",
+			"totalWordCount" : 37716,
+			"uniqueWordCount" : 2848,
+			"ratio" : 0.07551171916428041
+		}
+	},
+	{
+		"_id" : "2Pac",
+		"value" : {
+			"artistName" : "2Pac",
+			"totalWordCount" : 79033,
+			"uniqueWordCount" : 6029,
+			"ratio" : 0.07628458998140017
+		}
+	},
+	{
+		"_id" : "The Beach Boys",
+		"value" : {
+			"artistName" : "The Beach Boys",
+			"totalWordCount" : 52831,
+			"uniqueWordCount" : 4132,
+			"ratio" : 0.07821165603528231
+		}
+	},
+	{
+		"_id" : "The Kinks",
+		"value" : {
+			"artistName" : "The Kinks",
+			"totalWordCount" : 59881,
+			"uniqueWordCount" : 4748,
+			"ratio" : 0.07929059300946878
+		}
+	},
+	{
+		"_id" : "Bob Marley & The Wailers",
+		"value" : {
+			"artistName" : "Bob Marley & The Wailers",
+			"totalWordCount" : 30224,
+			"uniqueWordCount" : 2507,
+			"ratio" : 0.08294732662784542
+		}
+	},
+	{
+		"_id" : "Johnny Cash",
+		"value" : {
+			"artistName" : "Johnny Cash",
+			"totalWordCount" : 70240,
+			"uniqueWordCount" : 5836,
+			"ratio" : 0.0830865603644647
+		}
+	},
+	{
+		"_id" : "Otis Redding",
+		"value" : {
+			"artistName" : "Otis Redding",
+			"totalWordCount" : 15258,
+			"uniqueWordCount" : 1302,
+			"ratio" : 0.08533228470310657
+		}
+	},
+	{
+		"_id" : "Bruce Springsteen",
+		"value" : {
+			"artistName" : "Bruce Springsteen",
+			"totalWordCount" : 59837,
+			"uniqueWordCount" : 5145,
+			"ratio" : 0.08598358874943597
+		}
+	},
+	{
+		"_id" : "Janis Joplin",
+		"value" : {
+			"artistName" : "Janis Joplin",
+			"totalWordCount" : 13191,
+			"uniqueWordCount" : 1181,
+			"ratio" : 0.08953074065650822
+		}
+	},
+	{
+		"_id" : "Marvin Gaye",
+		"value" : {
+			"artistName" : "Marvin Gaye",
+			"totalWordCount" : 21286,
+			"uniqueWordCount" : 1908,
+			"ratio" : 0.08963638071972188
+		}
+	},
+	{
+		"_id" : "Elton John",
+		"value" : {
+			"artistName" : "Elton John",
+			"totalWordCount" : 58080,
+			"uniqueWordCount" : 5287,
+			"ratio" : 0.09102961432506887
+		}
+	},
+	{
+		"_id" : "Aerosmith",
+		"value" : {
+			"artistName" : "Aerosmith",
+			"totalWordCount" : 39016,
+			"uniqueWordCount" : 3555,
+			"ratio" : 0.0911164650399836
+		}
+	},
+	{
+		"_id" : "Queen",
+		"value" : {
+			"artistName" : "Queen",
+			"totalWordCount" : 34102,
+			"uniqueWordCount" : 3120,
+			"ratio" : 0.09149023517682248
+		}
+	},
+	{
+		"_id" : "Nine Inch Nails",
+		"value" : {
+			"artistName" : "Nine Inch Nails",
+			"totalWordCount" : 18830,
+			"uniqueWordCount" : 1735,
+			"ratio" : 0.09214020180562932
+		}
+	},
+	{
+		"_id" : "Lynyrd Skynyrd",
+		"value" : {
+			"artistName" : "Lynyrd Skynyrd",
+			"totalWordCount" : 24089,
+			"uniqueWordCount" : 2223,
+			"ratio" : 0.09228278467350243
+		}
+	},
+	{
+		"_id" : "U2",
+		"value" : {
+			"artistName" : "U2",
+			"totalWordCount" : 32133,
+			"uniqueWordCount" : 2972,
+			"ratio" : 0.09249058600192948
+		}
+	},
+	{
+		"_id" : "Black Sabbath",
+		"value" : {
+			"artistName" : "Black Sabbath",
+			"totalWordCount" : 27455,
+			"uniqueWordCount" : 2541,
+			"ratio" : 0.09255144782371152
+		}
+	},
+	{
+		"_id" : "David Bowie",
+		"value" : {
+			"artistName" : "David Bowie",
+			"totalWordCount" : 54944,
+			"uniqueWordCount" : 5180,
+			"ratio" : 0.09427781013395457
+		}
+	},
+	{
+		"_id" : "Muddy Waters",
+		"value" : {
+			"artistName" : "Muddy Waters",
+			"totalWordCount" : 13615,
+			"uniqueWordCount" : 1285,
+			"ratio" : 0.09438119720896071
+		}
+	},
+	{
+		"_id" : "Neil Young",
+		"value" : {
+			"artistName" : "Neil Young",
+			"totalWordCount" : 36148,
+			"uniqueWordCount" : 3520,
+			"ratio" : 0.09737744826823061
+		}
+	},
+	{
+		"_id" : "Ray Charles",
+		"value" : {
+			"artistName" : "Ray Charles",
+			"totalWordCount" : 21171,
+			"uniqueWordCount" : 2074,
+			"ratio" : 0.09796419630626801
+		}
+	},
+	{
+		"_id" : "Ramones",
+		"value" : {
+			"artistName" : "Ramones",
+			"totalWordCount" : 26382,
+			"uniqueWordCount" : 2615,
+			"ratio" : 0.09912061253885225
+		}
+	},
+	{
+		"_id" : "Bob Dylan",
+		"value" : {
+			"artistName" : "Bob Dylan",
+			"totalWordCount" : 53121,
+			"uniqueWordCount" : 5597,
+			"ratio" : 0.10536322734888275
+		}
+	},
+	{
+		"_id" : "The Temptations",
+		"value" : {
+			"artistName" : "The Temptations",
+			"totalWordCount" : 19281,
+			"uniqueWordCount" : 2037,
+			"ratio" : 0.10564804730045123
+		}
+	},
+	{
+		"_id" : "Stevie Wonder",
+		"value" : {
+			"artistName" : "Stevie Wonder",
+			"totalWordCount" : 17113,
+			"uniqueWordCount" : 1829,
+			"ratio" : 0.10687781218956349
+		}
+	},
+	{
+		"_id" : "Eminem",
+		"value" : {
+			"artistName" : "Eminem",
+			"totalWordCount" : 74167,
+			"uniqueWordCount" : 7941,
+			"ratio" : 0.10706918171154287
+		}
+	},
+	{
+		"_id" : "The Doors",
+		"value" : {
+			"artistName" : "The Doors",
+			"totalWordCount" : 18500,
+			"uniqueWordCount" : 1997,
+			"ratio" : 0.10794594594594595
+		}
+	},
+	{
+		"_id" : "James Taylor",
+		"value" : {
+			"artistName" : "James Taylor",
+			"totalWordCount" : 24387,
+			"uniqueWordCount" : 2674,
+			"ratio" : 0.10964858326157378
+		}
+	},
+	{
+		"_id" : "Frank Zappa",
+		"value" : {
+			"artistName" : "Frank Zappa",
+			"totalWordCount" : 73583,
+			"uniqueWordCount" : 8173,
+			"ratio" : 0.11107185083511137
+		}
+	},
+	{
+		"_id" : "Guns N' Roses",
+		"value" : {
+			"artistName" : "Guns N' Roses",
+			"totalWordCount" : 20028,
+			"uniqueWordCount" : 2227,
+			"ratio" : 0.11119432794088277
+		}
+	},
+	{
+		"_id" : "Led Zeppelin",
+		"value" : {
+			"artistName" : "Led Zeppelin",
+			"totalWordCount" : 18396,
+			"uniqueWordCount" : 2049,
+			"ratio" : 0.1113829093281148
+		}
+	},
+	{
+		"_id" : "R.E.M.",
+		"value" : {
+			"artistName" : "R.E.M.",
+			"totalWordCount" : 35133,
+			"uniqueWordCount" : 3947,
+			"ratio" : 0.11234451939771725
+		}
+	},
+	{
+		"_id" : "Grateful Dead",
+		"value" : {
+			"artistName" : "Grateful Dead",
+			"totalWordCount" : 32020,
+			"uniqueWordCount" : 3671,
+			"ratio" : 0.11464709556527171
+		}
+	},
+	{
+		"_id" : "The Allman Brothers Band",
+		"value" : {
+			"artistName" : "The Allman Brothers Band",
+			"totalWordCount" : 15576,
+			"uniqueWordCount" : 1792,
+			"ratio" : 0.1150487930148947
+		}
+	},
+	{
+		"_id" : "The Supremes",
+		"value" : {
+			"artistName" : "The Supremes",
+			"totalWordCount" : 12466,
+			"uniqueWordCount" : 1442,
+			"ratio" : 0.11567463500721964
+		}
+	},
+	{
+		"_id" : "John Lennon",
+		"value" : {
+			"artistName" : "John Lennon",
+			"totalWordCount" : 16839,
+			"uniqueWordCount" : 1955,
+			"ratio" : 0.11609953085100065
+		}
+	},
+	{
+		"_id" : "Roy Orbison",
+		"value" : {
+			"artistName" : "Roy Orbison",
+			"totalWordCount" : 14041,
+			"uniqueWordCount" : 1675,
+			"ratio" : 0.11929349761413005
+		}
+	},
+	{
+		"_id" : "Hank Williams",
+		"value" : {
+			"artistName" : "Hank Williams",
+			"totalWordCount" : 18325,
+			"uniqueWordCount" : 2201,
+			"ratio" : 0.12010914051841746
+		}
+	},
+	{
+		"_id" : "Nirvana",
+		"value" : {
+			"artistName" : "Nirvana",
+			"totalWordCount" : 13329,
+			"uniqueWordCount" : 1610,
+			"ratio" : 0.12078925650836522
+		}
+	},
+	{
+		"_id" : "Jay-Z",
+		"value" : {
+			"artistName" : "Jay-Z",
+			"totalWordCount" : 52273,
+			"uniqueWordCount" : 6434,
+			"ratio" : 0.12308457521091194
+		}
+	},
+	{
+		"_id" : "The Yardbirds",
+		"value" : {
+			"artistName" : "The Yardbirds",
+			"totalWordCount" : 10803,
+			"uniqueWordCount" : 1425,
+			"ratio" : 0.1319078033879478
+		}
+	},
+	{
+		"_id" : "Eagles",
+		"value" : {
+			"artistName" : "Eagles",
+			"totalWordCount" : 16522,
+			"uniqueWordCount" : 2190,
+			"ratio" : 0.13255053867570513
+		}
+	},
+	{
+		"_id" : "Santana",
+		"value" : {
+			"artistName" : "Santana",
+			"totalWordCount" : 15092,
+			"uniqueWordCount" : 2004,
+			"ratio" : 0.1327855817651736
+		}
+	},
+	{
+		"_id" : "NOFX",
+		"value" : {
+			"artistName" : "NOFX",
+			"totalWordCount" : 35454,
+			"uniqueWordCount" : 4793,
+			"ratio" : 0.1351892593219383
+		}
+	},
+	{
+		"_id" : "The Byrds",
+		"value" : {
+			"artistName" : "The Byrds",
+			"totalWordCount" : 18074,
+			"uniqueWordCount" : 2526,
+			"ratio" : 0.1397587695031537
+		}
+	},
+	{
+		"_id" : "The Band",
+		"value" : {
+			"artistName" : "The Band",
+			"totalWordCount" : 17352,
+			"uniqueWordCount" : 2500,
+			"ratio" : 0.14407561088059012
+		}
+	},
+	{
+		"_id" : "Beastie Boys",
+		"value" : {
+			"artistName" : "Beastie Boys",
+			"totalWordCount" : 35705,
+			"uniqueWordCount" : 5184,
+			"ratio" : 0.14518974933482706
+		}
+	},
+	{
+		"_id" : "Jerry Lee Lewis",
+		"value" : {
+			"artistName" : "Jerry Lee Lewis",
+			"totalWordCount" : 10084,
+			"uniqueWordCount" : 1469,
+			"ratio" : 0.14567631892106306
+		}
+	},
+	{
+		"_id" : "Run-D.M.C.",
+		"value" : {
+			"artistName" : "Run-D.M.C.",
+			"totalWordCount" : 22418,
+			"uniqueWordCount" : 3270,
+			"ratio" : 0.1458649299669908
+		}
+	},
+	{
+		"_id" : "The Police",
+		"value" : {
+			"artistName" : "The Police",
+			"totalWordCount" : 11377,
+			"uniqueWordCount" : 1678,
+			"ratio" : 0.1474905511118924
+		}
+	},
+	{
+		"_id" : "Joni Mitchell",
+		"value" : {
+			"artistName" : "Joni Mitchell",
+			"totalWordCount" : 33507,
+			"uniqueWordCount" : 4945,
+			"ratio" : 0.14758110245620318
+		}
+	},
+	{
+		"_id" : "The Clash",
+		"value" : {
+			"artistName" : "The Clash",
+			"totalWordCount" : 27417,
+			"uniqueWordCount" : 4069,
+			"ratio" : 0.14841156946420103
+		}
+	},
+	{
+		"_id" : "Radiohead",
+		"value" : {
+			"artistName" : "Radiohead",
+			"totalWordCount" : 11861,
+			"uniqueWordCount" : 1824,
+			"ratio" : 0.15378130005901694
+		}
+	},
+	{
+		"_id" : "Elvis Costello",
+		"value" : {
+			"artistName" : "Elvis Costello",
+			"totalWordCount" : 22677,
+			"uniqueWordCount" : 3534,
+			"ratio" : 0.15584071967191426
+		}
+	},
+	{
+		"_id" : "Chuck Berry",
+		"value" : {
+			"artistName" : "Chuck Berry",
+			"totalWordCount" : 12450,
+			"uniqueWordCount" : 1952,
+			"ratio" : 0.1567871485943775
+		}
+	},
+	{
+		"_id" : "Public Enemy",
+		"value" : {
+			"artistName" : "Public Enemy",
+			"totalWordCount" : 25533,
+			"uniqueWordCount" : 4005,
+			"ratio" : 0.15685583362707084
+		}
+	},
+	{
+		"_id" : "Tool",
+		"value" : {
+			"artistName" : "Tool",
+			"totalWordCount" : 11798,
+			"uniqueWordCount" : 1902,
+			"ratio" : 0.16121376504492288
+		}
+	},
+	{
+		"_id" : "Dr. Dre",
+		"value" : {
+			"artistName" : "Dr. Dre",
+			"totalWordCount" : 16234,
+			"uniqueWordCount" : 2726,
+			"ratio" : 0.16791918196377972
+		}
+	},
+	{
+		"_id" : "Pink Floyd",
+		"value" : {
+			"artistName" : "Pink Floyd",
+			"totalWordCount" : 16854,
+			"uniqueWordCount" : 2894,
+			"ratio" : 0.17170997982674735
+		}
+	},
+	{
+		"_id" : "Patti Smith",
+		"value" : {
+			"artistName" : "Patti Smith",
+			"totalWordCount" : 12053,
+			"uniqueWordCount" : 2077,
+			"ratio" : 0.17232224342487348
+		}
+	},
+	{
+		"_id" : "The Velvet Underground",
+		"value" : {
+			"artistName" : "The Velvet Underground",
+			"totalWordCount" : 11073,
+			"uniqueWordCount" : 1948,
+			"ratio" : 0.17592341732141245
+		}
+	}
+];
