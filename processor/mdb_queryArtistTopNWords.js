9c41f4f1ebd4d80c944f64738fc150f663dbef29
notrom <tim.ian.morton@gmail.com>
2014-06-24 21:52:07 +1200

    Added topN data query

---
+++ processor/mdb_queryArtistTopNWords.js
@@ -0,0 +1,30 @@
+//db.artistWords.find({"value.artistName": "The Velvet Underground"}).map(
+//		function(o){
+//			var topN = [];
+//			for (var i in o.value.wordCounts) {
+//				if (o.value.wordCounts.hasOwnProperty(i)) {
+//					topN.push([i, o.value.wordCounts[i]]);
+//				}
+//			}
+//			topN.sort(function(a, b) {return b[1] - a[1]});
+//			return topN.slice(0,10);
+//		});
+		
+db.artistWords.group(
+   {
+    key: { _id: 1 },
+	//cond: { _id: { $eq: "NOFX" } },
+    reduce: function( curr, result ) {
+				result.artistName = curr.value.artistName;
+				for (var i in curr.value.wordCounts) {
+					if (curr.value.wordCounts.hasOwnProperty(i)) {
+						result.topWords.push({word:i, count:curr.value.wordCounts[i]});
+					}
+				}
+            },
+    initial: { artistName: "", topWords: [] },
+    finalize: function(result) {
+				result.topWords.sort(function(a, b) {return b.count - a.count});
+				result.topWords = result.topWords.slice(0,20);
+              }
+   });
