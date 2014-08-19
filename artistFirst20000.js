89b022d930868eb2044721ac316310af62690206
notrom <tim.ian.morton@gmail.com>
2014-08-19 23:20:28 +1200

    Added new page for only the first 20000 words for each artist

---
+++ artistFirst20000.js
@@ -0,0 +1,317 @@
+var artistFirst20000 = [
+	{
+		"artistName" : "NOFX",
+		"totalWordCount" : 35454,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3217,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Metallica",
+		"totalWordCount" : 39317,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2409,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Bob Dylan",
+		"totalWordCount" : 53121,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3243,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Foo Fighters",
+		"totalWordCount" : 24343,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1681,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "The Beatles",
+		"totalWordCount" : 55745,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1450,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Elvis Presley",
+		"totalWordCount" : 129092,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1820,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "The Rolling Stones",
+		"totalWordCount" : 111715,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1628,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Jimi Hendrix",
+		"totalWordCount" : 22426,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1337,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Ray Charles",
+		"totalWordCount" : 21171,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2008,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "The Beach Boys",
+		"totalWordCount" : 52831,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2333,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Marvin Gaye",
+		"totalWordCount" : 21286,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1852,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "U2",
+		"totalWordCount" : 32133,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2248,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Bruce Springsteen",
+		"totalWordCount" : 59837,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2936,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Ramones",
+		"totalWordCount" : 26382,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2115,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "The Clash",
+		"totalWordCount" : 27417,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3365,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Johnny Cash",
+		"totalWordCount" : 70240,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2617,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Neil Young",
+		"totalWordCount" : 36148,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2522,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "James Brown",
+		"totalWordCount" : 36435,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1170,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Aretha Franklin",
+		"totalWordCount" : 26019,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1764,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Bob Marley & The Wailers",
+		"totalWordCount" : 30224,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1922,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Madonna",
+		"totalWordCount" : 37716,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2045,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Michael Jackson",
+		"totalWordCount" : 38321,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1899,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "David Bowie",
+		"totalWordCount" : 54944,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3083,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Van Morrison",
+		"totalWordCount" : 58863,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2131,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Public Enemy",
+		"totalWordCount" : 25533,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3434,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Run-D.M.C.",
+		"totalWordCount" : 22418,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3042,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Elton John",
+		"totalWordCount" : 58080,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2911,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Queen",
+		"totalWordCount" : 34102,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2453,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Eric Clapton",
+		"totalWordCount" : 35491,
+		"allWords" : [ ],
+		"uniqueWordCount" : 1927,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Grateful Dead",
+		"totalWordCount" : 32020,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2828,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Joni Mitchell",
+		"totalWordCount" : 33507,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3429,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "The Kinks",
+		"totalWordCount" : 59881,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2294,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Frank Zappa",
+		"totalWordCount" : 73583,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3170,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Beastie Boys",
+		"totalWordCount" : 35705,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3478,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Elvis Costello",
+		"totalWordCount" : 22677,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3239,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Eminem",
+		"totalWordCount" : 74167,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3644,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "James Taylor",
+		"totalWordCount" : 24387,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2347,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Black Sabbath",
+		"totalWordCount" : 27455,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2205,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "2Pac",
+		"totalWordCount" : 79033,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2963,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Jay-Z",
+		"totalWordCount" : 52273,
+		"allWords" : [ ],
+		"uniqueWordCount" : 3736,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Guns N' Roses",
+		"totalWordCount" : 20028,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2227,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Lynyrd Skynyrd",
+		"totalWordCount" : 24089,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2039,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "R.E.M.",
+		"totalWordCount" : 35133,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2711,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Aerosmith",
+		"totalWordCount" : 39016,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2402,
+		"smryWordCount" : 20000
+	},
+	{
+		"artistName" : "Prince",
+		"totalWordCount" : 67590,
+		"allWords" : [ ],
+		"uniqueWordCount" : 2193,
+		"smryWordCount" : 20000
+	}
+];
